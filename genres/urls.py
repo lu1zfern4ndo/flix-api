1f261f9b069dcb16e8f0701071e9f8be2c5aeb87
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-20 17:06:29 -0300

    Adicionado urls que lista detalhe do genero

--- genres/urls.py
+++ genres/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.genre_create_list, name='genre')
+    path('', views.genre_create_list, name='genre'),
+    path('<int:pk>', views.genre_detail, name='genre'),
 ]
