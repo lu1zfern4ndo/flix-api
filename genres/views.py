43bacb83cfb14d7298788f7ce7c4d46593a5aff5
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-20 16:51:11 -0300

    Alterado nome da função e criado inserção de gêneres

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,19 @@
+import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
 from .models import Genre
 
 
-def genre_list(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse({'genres': data})
+@csrf_exempt
+@require_POST
+def genre_create_list(request):
+    if request.method == 'GET':
+        genres = Genre.objects.all()
+        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+        return JsonResponse({'genres': data})
+    elif request.method == 'POST':
+        data = json.loads(request.body.decode('utf-8'))
+        new_genre = Genre(name=data.get('name'))
+        new_genre.save()
+        return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
