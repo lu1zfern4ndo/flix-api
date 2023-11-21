9829b7939c45888520cb9079a63d1dd109014080
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-21 16:48:03 -0300

    Criado CRUD para actors

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Actor
+from .serializers import ActorSerializer
 
-# Create your views here.
+
+class ActorCreateList(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
+
+
+class ActorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = ActorSerializer
