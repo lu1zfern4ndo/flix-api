3bd47473c318f1e0a60c5d62c24e2364d78afdb3
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-21 14:44:21 -0300

    Alterado nome da rota que altera, lista e exclui um gênero

--- genres/urls.py
+++ genres/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.GenreCreateList.as_view(), name='genre'),
-    path('<int:pk>/', views.genre_detail_update_delete, name='genre'),
+    path('', views.GenreCreateList.as_view(), name='genre_create_list'),
+    path('<int:pk>/', views.GenreRetrieveUpdateDestroy.as_view(), name='genre_retrieve_update_destroy'),
 ]
