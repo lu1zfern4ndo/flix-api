8c3883277c4830db781d2e778f23497e5cd3fd93
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-21 14:46:15 -0300

    Criada classe que lista, altera e exclui um gênero

--- genres/views.py
+++ genres/views.py
@@ -12,6 +12,11 @@ class GenreCreateList(generics.ListCreateAPIView):
     serializer_class = GenreSerializer
 
 
+class GenreRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
 # @csrf_exempt
 # def genre_create_list(request):
 #     if request.method == 'GET':
@@ -25,17 +30,17 @@ class GenreCreateList(generics.ListCreateAPIView):
 #         return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
 
 
-@csrf_exempt
-def genre_detail_update_delete(request, pk):
-    genre = get_object_or_404(Genre, pk=pk)
-    if request.method == 'GET':
-        data = {'id': genre.id, 'name': genre.name}
-        return JsonResponse(data)
-    elif request.method == 'PUT':
-        data = json.loads(request.body.decode('utf-8'))
-        genre.name = data.get('name')
-        genre.save()
-        return JsonResponse({'id': genre.id, 'name': genre.name})
-    elif request.method == 'DELETE':
-        genre.delete()
-        return JsonResponse({'message': 'Gênero excluído com sucesso'}, status=204)
+# @csrf_exempt
+# def genre_detail_update_delete(request, pk):
+#     genre = get_object_or_404(Genre, pk=pk)
+#     if request.method == 'GET':
+#         data = {'id': genre.id, 'name': genre.name}
+#         return JsonResponse(data)
+#     elif request.method == 'PUT':
+#         data = json.loads(request.body.decode('utf-8'))
+#         genre.name = data.get('name')
+#         genre.save()
+#         return JsonResponse({'id': genre.id, 'name': genre.name})
+#     elif request.method == 'DELETE':
+#         genre.delete()
+        # return JsonResponse({'message': 'Gênero excluído com sucesso'}, status=204)
