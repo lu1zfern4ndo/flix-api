b079fd3bb8fcd969259825108b90488e99eb7897
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-21 11:18:21 -0300

    Criada classe que utiliza CBV para criar e listar gêneros

--- genres/views.py
+++ genres/views.py
@@ -2,20 +2,27 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework import generics
 from .models import Genre
+from .serializers import GenreSerializer
 
 
-@csrf_exempt
-def genre_create_list(request):
-    if request.method == 'GET':
-        genres = Genre.objects.all()
-        data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-        return JsonResponse({'genres': data})
-    elif request.method == 'POST':
-        data = json.loads(request.body.decode('utf-8'))
-        new_genre = Genre(name=data.get('name'))
-        new_genre.save()
-        return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
+class GenreCreateList(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+# @csrf_exempt
+# def genre_create_list(request):
+#     if request.method == 'GET':
+#         genres = Genre.objects.all()
+#         data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+#         return JsonResponse({'genres': data})
+#     elif request.method == 'POST':
+#         data = json.loads(request.body.decode('utf-8'))
+#         new_genre = Genre(name=data.get('name'))
+#         new_genre.save()
+#         return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
 
 
 @csrf_exempt
