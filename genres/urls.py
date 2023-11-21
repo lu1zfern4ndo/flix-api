5a0f63a78e5a8071e425ce7ecadc82977d5db7b1
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-21 11:17:44 -0300

    Alterado url de criar e listar os gêneros usando CBV

--- genres/urls.py
+++ genres/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.genre_create_list, name='genre'),
+    path('', views.GenreCreateList.as_view(), name='genre'),
     path('<int:pk>/', views.genre_detail_update_delete, name='genre'),
 ]
