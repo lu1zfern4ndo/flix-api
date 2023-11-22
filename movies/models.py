127f46da2922c6fb36407f4a847da16b3bbc1497
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-22 15:07:45 -0300

    criado modelo da app movies

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from actors.models import Actor
+from genres.models import Genre
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=500)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movie')
+    release_date = models.DateField(null=True, blank=True)
+    actors = models.ManyToManyField(Actor, related_name='movie')
+
+    def __str__(self) -> str:
+        return self.title
