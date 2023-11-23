d331aa5f8517f6d323ca7c606e51a56939a61322
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-23 13:27:29 -0300

    Criado da para realização do CRUD da app de reviews

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Review
+from .serializers import ReviewSerializer
 
-# Create your views here.
+
+class ReviewsListCreate(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ReviewRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
