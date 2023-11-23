3e2f507fc9854f4347d6e75537a7e20fe0065dbd
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-23 14:41:06 -0300

    Verisonado urls do projeto

--- actors/urls.py
+++ actors/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ActorCreateList.as_view(), name='actor-list-create-view'),
-    path('<int:pk>/', views.ActorRetrieveUpdateDestroy.as_view(), name='actor-detail-view')
+    path('actors/', views.ActorCreateList.as_view(), name='actor-list-create-view'),
+    path('actors/<int:pk>/', views.ActorRetrieveUpdateDestroy.as_view(), name='actor-detail-view')
 ]
--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ReviewsListCreate.as_view(), name='reviews-list-create-view'),
-    path('<int:pk>/', views.ReviewRetrieveUpdateDestroy.as_view(), name='reviews-detail-view')
+    path('reviews/', views.ReviewsListCreate.as_view(), name='reviews-list-create-view'),
+    path('reviews/<int:pk>/', views.ReviewRetrieveUpdateDestroy.as_view(), name='reviews-detail-view')
 ]
