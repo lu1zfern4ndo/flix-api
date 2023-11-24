979185c583c879144af25f35e075cf57ca304c19
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-24 14:10:49 -0300

    Adicionado validação de ano e resumo para salvar um movie

--- movies/serializers.py
+++ movies/serializers.py
@@ -5,4 +5,14 @@ from .models import Movie
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
-        fields = ['title', 'genre', 'release_date', 'actors']
+        fields = '__all__'
+
+    def validate_release_date(self, value):
+        if value.year < 1990:
+            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+        return value
+
+    def validate_resume(self, value):
+        if len(value) > 200:
+            raise serializers.ValidationError('Resumo não deve ser mais do que 200 caracteres.')
+        return value
