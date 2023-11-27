0de870a3b8e035e76e72de7f036b7ada7003e143
Luiz Fernando <luiz-fernando@outlook.com>
2023-11-27 10:21:59 -0300

    Alterada lógica da média das estrelas usando aggregate e Avg

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,3 +1,4 @@
+from django.db.models import Avg
 from rest_framework import serializers
 from .models import Movie
 
@@ -10,32 +11,40 @@ class MovieSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def get_rate(self, obj):
-        reviews = obj.reviews.all()
+        rate = obj.reviews.aggregate(Avg('stars'))['stars__avg']
+        return round(rate, 1) if rate else 0
 
-        # 1º Veirficar se há alguma avaliação, senão houver não faz sentindo entrar nesse if
+        '''
+        # 1º Veirficar se há alguma avaliação, senão houver não faz sentindo
+        # entrar nesse if
         if reviews:
-            # No caso de haver alguma avaliação, então tá na hora de fazer a contagem para saber quantas estrelas existe cada filme
+            # No caso de haver alguma avaliação, então tá na hora de fazer a
+            # contagem para saber quantas estrelas existe cada filme
             # Somando as avaliações
             sum_reviews = 0
 
             # Iterando sobre cada avaliação
             for review in reviews:
                 sum_reviews += review.stars
-            
+
             reviews_count = reviews.count()
             return round(sum_reviews / reviews_count, 1)
-        
+
         return sum_reviews
-        
-        # Senão houver nenhuma avaliação, retorna direto um null de cara, nem perde tempo verficando
+
+        # Senão houver nenhuma avaliação, retorna direto um null de cara,
+        # nem perde tempo verficando
         return None
+        '''
 
     def validate_release_date(self, value):
         if value.year < 1900:
-            raise serializers.ValidationError('A data de lançamento não pode ser anterior a 1990.')
+            raise serializers.ValidationError(
+                'A data de lançamento não pode ser anterior a 1990.')
         return value
 
     def validate_resume(self, value):
         if len(value) > 500:
-            raise serializers.ValidationError('Resumo não deve ser mais do que 200 caracteres.')
+            raise serializers.ValidationError(
+                'Resumo não deve ser mais do que 200 caracteres.')
         return value
