9d72376e7fe677bc09c511c0b79ec24b1ddd2f78
Luiz Fernando <luiz-fernando@outlook.com>
2023-12-05 13:14:11 -0300

    Configurado autenticação por JWT

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,18 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Movie
 from .serializers import MovieSerializer
 
 
 class MovieCreateList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
 
 class MovieRetrieveUpdateDetroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
