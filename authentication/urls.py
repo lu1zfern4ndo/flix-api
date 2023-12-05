350fc7145a0e9a754f059a4b3246ddeefff2b8be
Luiz Fernando <luiz-fernando@outlook.com>
2023-12-05 13:15:02 -0300

    Criada autenticação por JWT

---
+++ authentication/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('authentication/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('authentication/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+]
