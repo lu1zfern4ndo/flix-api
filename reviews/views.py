8c06d579875191baff30fb640b57d9aea17c084b
Luiz Fernando <luiz-fernando@outlook.com>
2023-12-05 13:14:31 -0300

    Configurado autenticação por JWT

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,18 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Review
 from .serializers import ReviewSerializer
 
 
 class ReviewsListCreate(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
 class ReviewRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
