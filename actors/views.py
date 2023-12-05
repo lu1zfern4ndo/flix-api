3ac36b7ed369b263434fdae0c51f03c1a7739850
Luiz Fernando <luiz-fernando@outlook.com>
2023-12-05 13:13:36 -0300

    Configurado autenticação por JWT

--- actors/views.py
+++ actors/views.py
@@ -1,13 +1,18 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Actor
 from .serializers import ActorSerializer
 
 
 class ActorCreateList(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
 
 class ActorRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
